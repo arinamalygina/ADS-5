ca3059efd48b9d33f54b0e64dfb622f95d0d356e
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-04-11 10:55:31 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,17 +3,17 @@
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
-template <class T, int size> 
-class TStack { 
-private: 
-    T *stackPtr; 
-    int top; 
-public: 
-    TStack(); 
-    ~TStack(); 
-    void push(const T &value); 
-    T pop(); 
-    bool isEmpty(); 
-}; 
+template <class T, int size>
+class TStack {
+private:
+    T *stackPtr;
+    int top;
+public:
+    TStack();
+    ~TStack();
+    void push(const T &value);
+    T pop();
+    bool isEmpty();
+};
 
 #endif  // INCLUDE_TSTACK_H_
