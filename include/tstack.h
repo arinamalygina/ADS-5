011d959fe05e8c66cda67983be8b266ddf4aa04e
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-04-13 12:55:10 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,19 +1,31 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template <typename T, int size>
-template <class T, int size>
 class TStack {
  private:
-    T *stackPtr;
-    int top;
+  T arr[100];
+  int top;
+
  public:
-    TStack();
-    ~TStack();
-    void push(const T &value);
-    T pop();
-    bool isEmpty();
+  TStack() : top(-1) {}
+  void push(T value) {
+    if (!isFull())
+      arr[++top] = value;
+    else
+      throw std::string("Full");
+  }
+  T get() const { return arr[top]; }
+  T pop() {
+    if (isEmpty())
+      throw std::string("Empty");
+    else
+      return arr[top--];
+  }
+  bool isEmpty() const { return top == -1; }
+  bool isFull() const { return top == size - 1; }
 };
 
 #endif  // INCLUDE_TSTACK_H_
